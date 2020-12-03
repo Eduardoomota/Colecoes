dc89f4109f7c5b489a49f41f6d4e23a79f15d088
eduardowtmota <eduardowtmota@gmail.com>
2020-12-03 09:14:23 -0300

    fix: remove states unnecessary

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,4 +1,3 @@
-import { Fragment, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 // COMPONENTS
@@ -10,29 +9,21 @@ import Pokemon from "../pages/Pokemon";
 import Favorites from "../pages/Favorites";
 
 const Routes = ({ setTheme }) => {
-  const [addFavorite, setAddFavorite] = useState([]);
-
   return (
-    <Fragment>
+    <>
       <Header setTheme={setTheme} />
       <Switch>
         <Route exact path="/">
-          <RickAndMorty
-            setAddFavorite={setAddFavorite}
-            addFavorite={addFavorite}
-          />
+          <RickAndMorty />
         </Route>
         <Route exact path="/pokemon">
-          <Pokemon setAddFavorite={setAddFavorite} addFavorite={addFavorite} />
+          <Pokemon />
         </Route>
         <Route exact path="/favorites">
-          <Favorites
-            addFavorite={addFavorite}
-            setAddFavorite={setAddFavorite}
-          />
+          <Favorites />
         </Route>
       </Switch>
-    </Fragment>
+    </>
   );
 };
 
